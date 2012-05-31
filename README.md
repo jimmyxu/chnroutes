fdf75f20f23cee3e11e90aabfa06d1f97b33c59d
Jimmy Xu <me@jimmyxu.org>
2012-05-31 08:08:00 +0000

    old: Add native method back to support platforms w/o iproute2

--- README.md
+++ README.md
@@ -13,15 +13,21 @@
 
 ## OpenVPN
 
-### `iproute2`
+### iproute2
 
 1. 执行`python chnroutes.py`，这将生成`vpn-up.sh`和`vpn-down.sh`两个文件；
 2. 将上述两个文件移入`/etc/openvpn/`中，并加上执行权限；
 3. 在OpenVPN配置文件中加入：
-    script-security 2
+```    script-security 2
     up vpn-up.sh
-    down vpn-down.sh
+    down vpn-down.sh```
 4. 重新连接VPN，观察日志测试。
+
+### 自带方式
+
+1. 执行`python chnroutes.py -p old`, 这将生成`routes.txt`文本文件；
+2. 将该文件内容加在OpenVPN配置文件的尾部；
+3. 重新连接VPN，观察日志测试。
 
 ## PPTP
 ### Mac OSX
@@ -45,6 +51,6 @@
 
 一些基于Linux系统的第三方路由器系统如: OpenWRT，DD-WRT，Tomato 都带有VPN(PPTP/Openvpn)客户端的， 也就是说，我们只需要在路由器进行VPN拨号，并利用本项目提供的路由表脚本就可以把VPN针对性翻墙扩展到整个局域网。当然，使用这个方式也是会带来副作用，即局域网的任何机器都不适合使用Emule或者BT等P2P下载软件。但对于那些不使用P2P，希望在路由器上设置针对性翻墙的用户，这方法十分有用，因为只需要一个VPN帐号，局域网内的所有机器，包括使用wifi的手机都能自动翻墙。相信配置方式请参考[Autoddvpn](http://code.google.com/p/autoddvpn/)项目。
 
-# 信息反馈
+## 信息反馈
 
 本项目的脚本都是在使用路由器进行拨号的情况下测试通过的，如果在其它拨号方式下，脚本不能运作，请添加新的Issue。另外，在配合OpenVPN使用的时候，可能会出现因为网络质量不好，OpenVPN非主动断开，这时候`vpn-down.sh`脚本也会被自动调用，但重新连上之后，可能会找不到默认路由而添加失败，这时候你可以通过停止OpenVPN，并手动设置好原来的默认路由再重新进行OpenVPN拨号。
--- chnroutes.py
+++ chnroutes.py
@@ -39,12 +39,17 @@ ip -batch - <<EOF
     upfile.close()
     downfile.close()
 
-    print """\
-Place vpn-{up,down}.sh into /etc/openvpn/, chmod +x, and add
-    script-security 2
-    up vpnup.sh
-    down vpndown.sh
-to your openvpn.conf."""
+def generate_old(metric):
+    results = fetch_ip_data()
+
+    rfile = open('routes.txt','w')
+
+    rfile.write('max-routes %d\n\n' % (len(results) + 20))
+
+    for ip, mask, _ in results:
+        rfile.write("route %s %s net_gateway %d\n" % (ip, mask, metric))
+
+    rfile.close()
 
 def generate_linux(metric):
     results = fetch_ip_data()
@@ -91,9 +96,6 @@ EOF
 rm /tmp/vpn_oldgw
 ''')
 
-    print "For pptp only, please copy the file ip-pre-up to /etc/ppp/," \
-          "and copy the file ip-down to /etc/ppp/ip-down.d/."
-
 def generate_mac(metric):
     results=fetch_ip_data()
 
@@ -135,9 +137,6 @@ OLDGW=`cat /tmp/pptp_oldgw`
     upfile.close()
     downfile.close()
 
-    print "For pptp on mac only, please copy ip-up and ip-down to the /etc/ppp folder," \
-          "don't forget to make them executable with the chmod command."
-
 def generate_win(metric):
     results = fetch_ip_data()
 
@@ -161,53 +160,6 @@ for /F "tokens=3" %%* in ('route print ^| findstr "\\<0.0.0.0\\>"') do set "gw=%
 
     upfile.close()
     downfile.close()
-
-#    up_vbs_wrapper=open('vpnup.vbs','w')
-#    up_vbs_wrapper.write('Set objShell = CreateObject("Wscript.shell")\ncall objShell.Run("vpnup.bat",0,FALSE)')
-#    up_vbs_wrapper.close()
-#    down_vbs_wrapper=open('vpndown.vbs','w')
-#    down_vbs_wrapper.write('Set objShell = CreateObject("Wscript.shell")\ncall objShell.Run("vpndown.bat",0,FALSE)')
-#    down_vbs_wrapper.close()
-
-    print "For pptp on windows only, run vpnup.bat before dialing to vpn," \
-          "and run vpndown.bat after disconnected from the vpn."
-
-def generate_android(metric):
-    results = fetch_ip_data()
-
-    upscript_header = """\
-#!/bin/sh
-alias nestat='/system/xbin/busybox netstat'
-alias grep='/system/xbin/busybox grep'
-alias awk='/system/xbin/busybox awk'
-alias route='/system/xbin/busybox route'
-
-OLDGW=`netstat -rn | grep ^0\.0\.0\.0 | awk '{print $2}'`
-"""
-
-    downscript_header = """\
-#!/bin/sh
-alias route='/system/xbin/busybox route'
-"""
-
-    upfile = open('vpnup.sh','w')
-    downfile = open('vpndown.sh','w')
-
-    upfile.write(upscript_header)
-    upfile.write('\n')
-    downfile.write(downscript_header)
-    downfile.write('\n')
-
-    for ip, mask, _ in results:
-        upfile.write('route add -net %s netmask %s gw $OLDGW\n' % (ip, mask))
-        downfile.write('route del -net %s netmask %s\n' % (ip, mask))
-
-    upfile.close()
-    downfile.close()
-
-    print "Old school way to call up/down script from openvpn client. " \
-          "use the regular openvpn 2.1 method to add routes if it's possible"
-
 
 def fetch_ip_data():
     url = 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
@@ -254,8 +206,7 @@ if __name__ == '__main__':
                         dest='platform',
                         default='openvpn',
                         nargs='?',
-                        help="Target platforms, it can be openvpn, mac, linux," 
-                        "win, android. openvpn by default.")
+                        help="Target platform, it can be openvpn, old,  mac, linux, win. openvpn by default.")
     parser.add_argument('-m','--metric',
                         dest='metric',
                         default=5,
@@ -267,14 +218,14 @@ if __name__ == '__main__':
 
     if args.platform.lower() == 'openvpn':
         generate_ovpn(args.metric)
+    elif args.platform.lower() == 'old':
+        generate_old(args.metric)
     elif args.platform.lower() == 'linux':
         generate_linux(args.metric)
     elif args.platform.lower() == 'mac':
         generate_mac(args.metric)
     elif args.platform.lower() == 'win':
         generate_win(args.metric)
-    elif args.platform.lower() == 'android':
-        generate_android(args.metric)
     else:
         print >>sys.stderr, "Platform %s is not supported." % args.platform
         exit(1)
